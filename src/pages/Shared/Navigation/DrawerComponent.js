2dfae5a0c1c0a69025d61cebfbec57dd407e0278
mdnajmul <mdnajmulovi@gmail.com>
2021-11-14 01:30:23 +0600

    responsive navigation complete

---
+++ src/pages/Shared/Navigation/DrawerComponent.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import {
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  makeStyles,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles(() => ({
+  link: {
+    textDecoration: "none",
+    color: "blue",
+    fontSize: "15px",
+    "&:hover": {
+      color: "yellow",
+    },
+  },
+  icon: {
+    color: "white",
+  },
+}));
+
+const DrawerComponent = () => {
+  const classes = useStyles();
+  const [openDrawer, setOpenDrawer] = useState(false);
+  return (
+    <>
+      <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
+        <List>
+          <ListItem onClick={() => setOpenDrawer(false)}>
+            <ListItemText>
+              <Link to="/" className={classes.link}>
+                HOME
+              </Link>
+            </ListItemText>
+          </ListItem>
+          <Divider />
+          <ListItem onClick={() => setOpenDrawer(false)}>
+            <ListItemText>
+              <Link to="/products" className={classes.link}>
+                ALL COLLECTION
+              </Link>
+            </ListItemText>
+          </ListItem>
+          <Divider />
+          <ListItem onClick={() => setOpenDrawer(false)}>
+            <ListItemText>
+              <Link to="/dashboard" className={classes.link}>
+                DASHBOARD
+              </Link>
+            </ListItemText>
+          </ListItem>
+          <Divider />
+          <ListItem onClick={() => setOpenDrawer(false)}>
+            <ListItemText>
+              <Link to="/login" className={classes.link}>
+                LOGIN
+              </Link>
+            </ListItemText>
+          </ListItem>
+          <Divider />
+        </List>
+      </Drawer>
+      <IconButton
+        onClick={() => setOpenDrawer(!openDrawer)}
+        className={classes.icon}
+      >
+        <MenuIcon />
+      </IconButton>
+    </>
+  );
+};
+
+export default DrawerComponent;
--- src/pages/Shared/Navigation/Navigation.js
+++ src/pages/Shared/Navigation/Navigation.js
@@ -1,49 +1,72 @@
 import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  makeStyles,
+  useTheme,
+  useMediaQuery,
+  Button,
+} from "@material-ui/core";
 import { NavLink } from "react-router-dom";
+import DrawerComponent from "./DrawerComponent";
 
+const useStyles = makeStyles((theme) => ({
+  navlinks: {
+    marginLeft: theme.spacing(5),
+    display: "flex",
+  },
+  logo: {
+    flexGrow: "1",
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "15px",
+    "&:hover": {
+      color: "yellow",
+      borderBottom: "1px solid yellow",
+    },
+  },
+}));
+
+// marginLeft: theme.spacing(20),
 const Navigation = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Bicycle<span style={{ color: "red" }}>Mart</span>
-          </Typography>
-          <NavLink style={{ textDecoration: "none", color: "white" }} to="/">
-            <Button color="inherit">Home</Button>
-          </NavLink>
-          <NavLink
-            style={{ textDecoration: "none", color: "white" }}
-            to="/products"
-          >
-            <Button color="inherit">Products</Button>
-          </NavLink>
-          <NavLink
-            style={{ textDecoration: "none", color: "white" }}
-            to="/dashboard"
-          >
-            <Button color="inherit">Dashboard</Button>
-          </NavLink>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static">
+      <CssBaseline />
+      <Toolbar>
+        <Typography
+          variant="h6"
+          className={classes.logo}
+          component="div"
+          sx={{ flexGrow: 1 }}
+        >
+          Bicycle<span style={{ color: "red" }}>Mart</span>
+        </Typography>
+        {isMobile ? (
+          <DrawerComponent />
+        ) : (
+          <div className={classes.navlinks}>
+            <NavLink style={{ textDecoration: "none" }} to="/">
+              <Button className={classes.link}>Home</Button>
+            </NavLink>
+            <NavLink style={{ textDecoration: "none" }} to="/products">
+              <Button className={classes.link}>ALL COLLECTION</Button>
+            </NavLink>
+            <NavLink style={{ textDecoration: "none" }} to="/dashboard">
+              <Button className={classes.link}>Dashboard</Button>
+            </NavLink>
+            <Button className={classes.link}>Login</Button>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
 
