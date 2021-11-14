d39056fdbfb5bf8add88679159f4d9e90db31483
mdnajmul <mdnajmulovi@gmail.com>
2021-11-14 20:26:13 +0600

    registration page design complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home/Home";
 import Login from "./pages/Login/Login/Login";
+import Register from "./pages/Login/Register/Register";
 import Navigation from "./pages/Shared/Navigation/Navigation";
 
 function App() {
@@ -15,6 +16,10 @@ function App() {
           <Route exact path="/login">
             <Navigation></Navigation>
             <Login></Login>
+          </Route>
+          <Route exact path="/register">
+            <Navigation></Navigation>
+            <Register></Register>
           </Route>
         </Switch>
       </Router>
--- src/pages/Login/Register/Register.js
+++ src/pages/Login/Register/Register.js
@@ -1,10 +1,112 @@
-import React from "react";
+import { Box, Button, Grid, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
 
 const Register = () => {
+  const [registerData, setRegisterData] = useState();
+  const history = useHistory();
+  const user = useAuth();
+  const registerUser = useAuth();
+  const isLoading = useAuth();
+  const authError = useAuth();
+
+  const handleOnBlur = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+    const newRegisterData = { ...registerData };
+    newRegisterData[field] = value;
+    setRegisterData(newRegisterData);
+  };
+
+  const handleRegisterSubmit = (e) => {
+    registerUser(
+      registerData.email,
+      registerData.password,
+      registerData.name,
+      registerData.phone,
+      history
+    );
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h2>Register</h2>
-    </div>
+    <Box>
+      <Box sx={{ justifyContent: "center" }}>
+        <Grid container spacing={2}>
+          <Grid
+            item
+            className="login"
+            sx={{
+              mt: 8,
+              mx: "auto",
+              textAlign: "center",
+              alignItems: "center",
+            }}
+            xs={12}
+            md={6}
+          >
+            <Typography sx={{ mt: 4 }} variant="body1" gutterBottom>
+              <span
+                style={{ color: "green", fontWeight: "bold", fontSize: "24px" }}
+              >
+                Register
+              </span>
+            </Typography>
+            <form onSubmit={handleRegisterSubmit}>
+              <TextField
+                sx={{ width: "75%", m: 1 }}
+                id="standard-basic"
+                label="Your Name"
+                name="name"
+                onChange={handleOnBlur}
+                variant="standard"
+              />
+              <TextField
+                sx={{ width: "75%", m: 1 }}
+                id="standard-basic"
+                label="Your Email"
+                name="email"
+                onChange={handleOnBlur}
+                variant="standard"
+              />
+              <TextField
+                sx={{ width: "75%", m: 1 }}
+                id="standard-basic"
+                label="Your Password"
+                type="password"
+                name="password"
+                onChange={handleOnBlur}
+                variant="standard"
+              />
+              <TextField
+                sx={{ width: "75%", m: 1 }}
+                id="standard-basic"
+                label="Your Phone"
+                name="phone"
+                onChange={handleOnBlur}
+                variant="standard"
+              />
+
+              <Button
+                sx={{ width: "75%", mt: 2, mb: 1 }}
+                type="submit"
+                variant="contained"
+              >
+                Register
+              </Button>
+            </form>
+            <Typography sx={{ mt: 4 }} variant="body1" gutterBottom>
+              <span>
+                Already a member?{" "}
+                <NavLink style={{ textDecoration: "none" }} to="/login">
+                  Please Login
+                </NavLink>
+              </span>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
   );
 };
 
