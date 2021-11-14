5d019179517c5cc79799b944b0aefdbd701e3c0a
mdnajmul <mdnajmulovi@gmail.com>
2021-11-14 12:00:24 +0600

    Email/Password login register functionality,save &  update database user table

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import {
   getAuth,
-  signInWithPopup,
-  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
   onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  updateProfile,
   signOut,
 } from "firebase/auth";
 import initializeAuthentication from "../Firebase/firebase.init";
@@ -13,11 +16,12 @@ initializeAuthentication();
 const useFirebase = () => {
   const [user, setUser] = useState({});
   const [products, setProducts] = useState([]);
+  const [authError, setAuthError] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
   //get all products
   useEffect(() => {
-    fetch("./cycle-products.json")
+    fetch("http://localhost:5000/products")
       .then((res) => res.json())
       .then((data) => {
         setProducts(data);
@@ -27,16 +31,65 @@ const useFirebase = () => {
   const auth = getAuth();
   const googleProvider = new GoogleAuthProvider();
 
-  const signInUsingGoogle = () => {
+  const registerUser = (email, password, name, phone, history) => {
     setIsLoading(true);
-    return signInWithPopup(auth, googleProvider);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setAuthError("");
+        const newUser = { email, displayName: name, phoneNumber: phone };
+        setUser(newUser);
+        // save user to the database
+        saveUser(email, name, phone, "POST");
+        // send name to firebase after creation
+        updateProfile(auth.currentUser, {
+          displayName: name,
+          phoneNumber: phone,
+        })
+          .then(() => {})
+          .catch((error) => {});
+        history.replace("/");
+      })
+      .catch((error) => {
+        setAuthError(error.message);
+        console.log(error);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  const loginUser = (email, password, location, history) => {
+    setIsLoading(true);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const destination = location?.state?.from || "/";
+        history.replace(destination);
+        setAuthError("");
+      })
+      .catch((error) => {
+        setAuthError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  const signInUsingGoogle = (location, history) => {
+    setIsLoading(true);
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        saveUser(user.email, user.displayName, user.phoneNumber, "PUT");
+        setAuthError("");
+        const destination = location?.state?.from || "/";
+        history.replace(destination);
+      })
+      .catch((error) => {
+        setAuthError(error.message);
+      })
+      .finally(() => setIsLoading(false));
   };
 
   const logOut = () => {
     setIsLoading(true);
     signOut(auth)
       .then(() => {
-        sessionStorage.removeItem("email");
         setUser({});
       })
       .finally(() => setIsLoading(false));
@@ -54,8 +107,22 @@ const useFirebase = () => {
     return () => unsubscribed;
   }, [auth]);
 
+  const saveUser = (email, displayName, phoneNumber, method) => {
+    const user = { email, displayName, phoneNumber };
+    fetch("http://localhost:5000/users", {
+      method: method,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then();
+  };
+
   return {
     user,
+    authError,
+    registerUser,
+    loginUser,
     signInUsingGoogle,
     logOut,
     products,
