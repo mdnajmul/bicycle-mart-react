904d9ec08d81d2e134f0cd7bbec8f0322910fd03
mdnajmul <mdnajmulovi@gmail.com>
2021-11-19 11:16:03 +0600

    carosel banner bug fix

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,24 +1,19 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
+import banner1 from "../../../images/banner/banner-1.jpg";
+import banner2 from "../../../images/banner/banner-2.jpg";
+import banner3 from "../../../images/banner/banner-3.jpg";
+import banner4 from "../../../images/banner/banner-4.jpg";
+import banner5 from "../../../images/banner/banner-5.jpg";
 
 const Banner = () => {
   return (
     <Carousel indicators={false}>
-      <div>
-        <img style={{ width: "100%" }} src="./banner/banner-1.jpg" alt="" />
-      </div>
-      <div>
-        <img style={{ width: "100%" }} src="./banner/banner-2.jpg" alt="" />
-      </div>
-      <div>
-        <img style={{ width: "100%" }} src="./banner/banner-3.jpg" alt="" />
-      </div>
-      <div>
-        <img style={{ width: "100%" }} src="./banner/banner-4.jpg" alt="" />
-      </div>
-      <div>
-        <img style={{ width: "100%" }} src="./banner/banner-5.jpg" alt="" />
-      </div>
+      <img style={{ width: "100%", height: "300px" }} src={banner1} alt="" />
+      <img style={{ width: "100%", height: "300px" }} src={banner2} alt="" />
+      <img style={{ width: "100%", height: "300px" }} src={banner3} alt="" />
+      <img style={{ width: "100%", height: "300px" }} src={banner4} alt="" />
+      <img style={{ width: "100%", height: "300px" }} src={banner5} alt="" />
     </Carousel>
   );
 };
